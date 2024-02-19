47c170ca398fcaa241b29ff8cfd89e8d06e1cbb6
Dr. Ernesto Lee <socrates73@gmail.com>
2024-02-18 21:06:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key')
 client = OpenAI(api_key=openai_api_key)
 
 # Allow users to set parameters for the model
-@st.cache
 with st.sidebar:
     st.write("Set Model Parameters")
     temperature = st.slider("Temperature", 0.0, 2.0, 1.0)
